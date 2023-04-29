a23da7652938e8ea985c4169b5143527aa39cc28
Sehyo Chang <sehyo@fluvio.io>
2023-04-29 12:15:00 -0700

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,76 @@
-use fluvio_smartmodule::{smartmodule, eyre, Record, RecordData, Result};
-use serde::{Deserialize, Serialize};
-use std::sync::Mutex;
+use std::sync::atomic::{AtomicU32, Ordering};
+
 use once_cell::sync::OnceCell;
 
-#[derive(Default, Debug)]
+use fluvio_smartmodule::{
+    dataplane::smartmodule::SmartModuleExtraParams, eyre, smartmodule, Record, RecordData, Result,
+};
+use serde::Deserialize;
+
+// use u32 to represent the metric
+type METRIC = u32;
+
+#[derive(Default, Debug, Deserialize)]
 struct StarsForks {
-    stars: u32,
-    forks: u32,
+    stars: AtomicU32,
+    forks: AtomicU32,
+}
+
+impl StarsForks {
+    fn get_stars(&self) -> METRIC {
+        self.stars.load(Ordering::SeqCst)
+    }
+
+    fn set_stars(&self, new: METRIC) {
+        self.stars.store(new, Ordering::SeqCst);
+    }
+
+    fn get_forks(&self) -> METRIC {
+        self.forks.load(Ordering::SeqCst)
+    }
+
+    fn set_forks(&self, new: METRIC) {
+        self.forks.store(new, Ordering::SeqCst);
+    }
+
+    // generate emoji string based on the new stars and forks
+    fn update_and_generate_moji_string(&self, new: &GithubRecord) -> Option<String> {
+        if new.stars == 0 && new.forks == 0 {
+            // if no stars and forks, return None
+            None
+        } else if new.stars != self.get_stars() && new.stars != self.get_forks() {
+            // if both stars and forks are changed, generate new emoji on prev stats
+            let emoji = format!(
+                ":gitfork: {} \n:star2: {}",
+                self.get_forks(),
+                self.get_stars()
+            );
+            self.set_forks(new.forks);
+            self.set_stars(new.stars);
+            Some(emoji)
+        } else if new.forks != self.get_forks() {
+            // if only forks are changed, generate new emoji on prev stats
+            let emoji = format!(":gitfork: {}", self.get_forks());
+            self.set_forks(new.forks);
+            Some(emoji)
+        } else if new.stars != self.get_stars() {
+            let emoji = format!(":star2: {}", self.get_stars());
+            self.set_stars(new.stars);
+            Some(emoji)
+        } else {
+            // no changes
+            None
+        }
+    }
+}
+
+static STARS_FORKS: OnceCell<StarsForks> = OnceCell::new();
+
+#[smartmodule(init)]
+fn init(_params: SmartModuleExtraParams) -> Result<()> {
+    STARS_FORKS
+        .set(StarsForks::default())
+        .map_err(|err| eyre!("regex init: {:#?}", err))
 }
 
 #[derive(Default, Deserialize)]
@@ -15,54 +79,16 @@ pub struct GithubRecord {
     forks: u32,
 }
 
-#[derive(Default, Serialize)]
-pub struct GithubString {
-    result: String
-}
-
-fn stars_forks() -> &'static Mutex<StarsForks> {
-    static STARS_FORKS: OnceCell<Mutex<StarsForks>> = OnceCell::new();
-    STARS_FORKS.get_or_init(|| {
-        Mutex::new(StarsForks::default())
-    })
-}
-
-impl GithubRecord {
-    pub fn to_emoji_string(&self, stars: u32, forks: u32) -> Option<GithubString> {
-        if stars == 0 && forks == 0 {
-            None
-        } else if stars != self.stars && forks !=self.forks {
-            Some(GithubString::new(format!(":gitfork: {} \n:star2: {}", self.forks, self.stars )))
-        } else if forks != self.forks {
-            Some(GithubString::new(format!(":gitfork: {}", self.forks)))
-        } else if stars != self.stars {
-            Some(GithubString::new(format!(":star2: {}", self.stars)))
-        } else { // no changes
-            None
-        }
-    }
-}
-
-impl GithubString {
-    pub fn new(result: String) -> Self {
-        Self { result }
-    }
-}
-
 #[smartmodule(filter_map)]
 pub fn filter_map(record: &Record) -> Result<Option<(Option<RecordData>, RecordData)>> {
-    let record_json: GithubRecord = serde_json::from_slice(record.value.as_ref())?;
+    let new_data: GithubRecord = serde_json::from_slice(record.value.as_ref())?;
 
-    let star_forks_mutex = stars_forks();
-    let mut star_forks = star_forks_mutex.lock().map_err(|e| eyre!("lock poisioned {e}"))?;
-    let string_result = record_json.to_emoji_string(star_forks.stars, star_forks.forks);
+    let accumulator = STARS_FORKS.get().unwrap();
 
-    star_forks.stars = record_json.stars;
-    star_forks.forks = record_json.forks;
-    
-    if let Some (result) = string_result {
-        let output = serde_json::to_vec(&result)?;
-        Ok(Some((record.key.clone(), output.into()))) 
+    if let Some(emoji) = accumulator.update_and_generate_moji_string(&new_data) {
+        let output = serde_json::to_vec(&emoji)?;
+        // need to update
+        Ok(Some((record.key.clone(), output.into())))
     } else {
         Ok(None)
     }
